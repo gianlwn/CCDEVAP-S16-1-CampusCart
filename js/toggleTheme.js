131e2156afe83db7128d936a387e00babe544ddb
Sky Hannah Parado <sky.parado@gmail.com>
2026-06-09 13:45:27 +0800

    theme toggle

--- js/toggleTheme.js
+++ js/toggleTheme.js
@@ -1,1 +1,21 @@
+function storeTheme(){
+  
+  const saved = localStorage.getItem('theme')
+  
+  if (saved == 'darkMode') {
+    document.body.classList.add('darkMode');
+  }
+  
+}
+
+function toggleTheme(){
+  
+document.body.classList.toggle('darkMode');
+  
+const isDark = document.body.classList.contains('darkMode');
+  localStorage.setItem('theme', isDark ? 'darkMode' : 'lightMode');
+
+  document.getElementById('theme-toggle').textContent = isDark ? '☀️' : '🌙';
+}
+
 
