5a205d8da42ba00df79051913a8e3847f28f1625
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-06-13 01:06:27 +0800

    fixed the active state in the nav bar

--- js/components.js
+++ js/components.js
@@ -71,9 +71,10 @@ function loadSideNav() {
   document.getElementById("side-nav").innerHTML = html;
 }
 
-function loadAdminSideNav() {
-const html = `
+function loadAdminSideNav(page) {
+  const html = `
     <div class="nav-links">
+      <a href="adminDashboard.html" class="nav-item">Admin Dashboard</a> 
       <a href="listingApproval.html" class="nav-item">Listings Approval</a>
       <a href="reports.html" class="nav-item">Reports</a>
       <a href="users.html" class="nav-item">Users</a>
@@ -81,11 +82,17 @@ const html = `
       <a href="admins.html" class="nav-item">Admins</a>
     </div>
     <div class="sign-out-box">
-      <a href="" class="sign-out-btn">Sign Out</a>
+      <a href="#" class="sign-out-btn">Sign Out</a>
     </div>
   `;
 
-  document.getElementById("admin-side-nav").innerHTML = html;
+  const sidebar = document.getElementById("admin-side-nav");
+  sidebar.innerHTML = html;
+
+  const activeLink = sidebar.querySelector(`a[href="${page}"]`);
+  if (activeLink) {
+    activeLink.classList.add("active");
+  }
 }
 
 function createClaimedRow(item) {
