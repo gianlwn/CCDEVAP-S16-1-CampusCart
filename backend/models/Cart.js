b9f14886ed10ca98143fa0a6e7f3b9d630d4243e
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-06-28 16:23:07 +0800

    edited cart schema

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema(
   {
-    user_id: { type: String, required: true },
+    cart_id: { type: String, required: true, unique: true },
+    buyer_id: { type: String, required: true },
     listing_id: { type: String, required: true },
     quantity: { type: Number, default: 1 },
+    does_exist: { type: Boolean, default: true },
   },
   {
     timestamps: { createdAt: "added_at", updatedAt: false },
