99f2aa953d386799c794595d30ea00939eaec864
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-06-30 00:43:21 +0800

    updated db and fixed a bug

--- backend/utils/generateId.js
+++ backend/utils/generateId.js
@@ -1,7 +1,10 @@
 async function generateId(Model, field, prefix) {
   const regex = new RegExp(`^${prefix}\\d+$`);
-  const last = await Model.findOne({ [field]: regex }).sort({ [field]: -1 });
-  const lastNum = last ? parseInt(last[field].replace(prefix, "")) : 0;
-  return `${prefix}${lastNum + 1}`;
+  const docs = await Model.find({ [field]: regex }, { [field]: 1, _id: 0 });
+  const maxNum = docs.reduce((max, doc) => {
+    const n = parseInt(doc[field].replace(prefix, ""), 10);
+    return n > max ? n : max;
+  }, 0);
+  return `${prefix}${maxNum + 1}`;
 }
 module.exports = generateId;
