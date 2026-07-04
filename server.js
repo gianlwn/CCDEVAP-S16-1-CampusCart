dfa9f443726fe334d99759eba4ed858951bb79a5
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-07-04 19:13:21 +0800

    refactor: simplify server listen log message

--- server.js
+++ server.js
@@ -31,6 +31,4 @@ app.get("/", (req, res) =>
 );
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "localhost", () =>
-  console.log(`Server running on http://localhost:${PORT}`),
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
