eb0d131fb5956e10c5e83acc922a6afd3d94aaf0
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-07-13 13:27:55 +0800

    fix: update server log message to include 'localhost'

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.get("/", (req, res) =>
 );
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
