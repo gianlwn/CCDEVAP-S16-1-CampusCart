c2d4423246744da7f1ead54d0dce33eb625b428e
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-08-03 09:31:44 +0800

    Log CLIENT_ORIGIN instead of hardcoded localhost on server start

--- server.js
+++ server.js
@@ -114,4 +114,4 @@ app.get("/", (req, res) =>
 );
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at ${CLIENT_ORIGIN}`));
